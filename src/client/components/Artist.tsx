3625f81491a28f74cf836cbb32f526c778e5395e
yeahgc <nohypegc@protonmail.com>
2021-12-08 17:26:27 -0600

    yop

--- src/client/components/Artist.tsx
+++ src/client/components/Artist.tsx
@@ -1,12 +1,13 @@
-import { Link } from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
-import { useParams, useHistory } from 'react-router-dom';
-//import SingleArtist from "./SpotifyAPI";
+import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import moment from "moment";
 
+//import SingleArtist from "./SpotifyAPI";
 const Artist = (props: IArtistProps) => {
     const history = useHistory();
     const [musicians, setMusicians] = useState({});
-    const { musicianmid } = useParams<{ musicianmid: string }>()
+    const { musicianmid } = useParams<{ musicianmid: string }>();
 
     useEffect(() => {
         const fetchMusicians = async () => {
@@ -14,88 +15,92 @@ const Artist = (props: IArtistProps) => {
             const data = await res.json();
             setMusicians(data[0]);
         };
-
         fetchMusicians();
     }, []);
 
-    
-
     return (
-        <>
-            <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <img src="https://media.istockphoto.com/vectors/cartoon-rock-band-vector-vector-id621592618" className="d-block w-100" alt="sample band"></img>
-                    </div>
-                    <div className="carousel-item">
-                        <img src="https://previews.123rf.com/images/bowie15/bowie151303/bowie15130300131/18687759-group-of-musicians-playing-instruments.jpg" className="d-block w-100" alt="bandsample2"></img>
-                    </div>
-                    <div className="carousel-item">
-                        <img src="https://img.jakpost.net/c/2019/05/16/2019_05_16_72418_1558000777._large.jpg" className="d-block w-100" alt="bandinstruments"></img>
-                    </div>
-                    {/* <div>
-                        <Link to={`/admin/${props.musician.mid}`}>
-                            <button className="btn btn-sm btn-primary">Admin Options</button>
-                        </Link> */}
-                    {/* </div> */}
-                </div>
-            </div>
-            )
-            <div className="card m-3 d-flex justify-content-between align-items-center flex-row">
-                <div className="card-body">
-                    <h6 className="card-subtitle mb-2 text-muted">@{musicians?.mname}</h6>
-                    <h1 className="card-text">{musicians?.mband}</h1>
-                    <h1 className="card-text">{musicians?.mbio}</h1>
-                </div>
-                <div>
-                    {/* <Link to={`/admin/${musicians?.mid}`}>
-                        <button className="btn btn-sm btn-primary">Admin Options</button>
-                    </Link> */}
-                </div>
-            </div>
-
-            <div className="card m-3 d-flex justify-content-between align-items-center flex-row">
-                <div className="card-body">
-                    <h6 className="card-subtitle mb-2 text-muted">@{musicians?.mname}</h6>
-                    <h1 className="card-text">{musicians?.mdob}</h1>
-                    <h1 className="card-text">{musicians?.mzodiac}</h1>
-                    <h1 className="card-text">{musicians?.mhometown}</h1>
-                    <h1 className="card-text">{musicians?.mage}</h1>
-                </div>
-                
-                <div>
-                    {/* <Link to={`/admin/${props.musicians.mid}`}>
-                        <button className="btn btn-sm btn-primary">Admin Options</button>
-                    </Link> */}
-                </div>
-            </div>
-
-          
-                <div>
-            <iframe src={`https://open.spotify.com/embed/album/${musicians?.mspotifyid}`} width="500" height="380" frameBorder="0" allow="encrypted-media"></iframe>
-                </div>
-
-            {<><div id="social"> {'}'}
-                {<><a className="btn btn-primary" style={{ backgroundColor: "#3b5998" }} href="#!" role="button"><i className="fab fa-facebook-f"></i></a><a className="btn btn-primary" style={{ backgroundColor: "#55acee" }} href="#!" role="button"><i className="fab fa-twitter"></i></a></>}
-            </div><a className="btn btn-primary" style={{ backgroundColor: "#ac2bac" }} href="#!" role="button"><i className="fab fa-instagram"></i></a><a className="btn btn-primary" style={{ backgroundColor: "#ed302f" }} href="#!" role="button"><i className="fab fa-youtube"></i></a></>}
+        
+        <div className="container bio-container">
             
-        </>
+            <div className="row">
+                <div className="d-flex flex-column column h-100">
+                
+                    <img
+                        src="https://i1.sndcdn.com/avatars-000244528170-lwj2hc-t500x500.jpg"
+                        className="d-block w-100 mx-auto mb-3 band-img"
+                        alt="sample band"
+                    />
 
-    )
-}
+                    <div className="spotify-box">
+                        <iframe
+                            src={`https://open.spotify.com/embed/album/${musicians?.mspotifyid}`}
+                            width="400"
+                            height="500"
+                            frameBorder="0"
+                            allow="encrypted-media"
+                        ></iframe>
+                        <div className="social">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkBID9Fih7-sURQoZlBYnPmjiQAvnH0JzLsw&usqp=CAU" className="m-1" alt="" />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="d-flex flex-column column h-100">
+                    <div id="card-container">
+                        <div className="card m-3 d-flex justify-content-between align-items-center flex-row">
+                            <div className="card-body">
+                                <h6 className="card-subtitle mb-2 text-muted">
+                                    @{musicians?.mname}
+                                </h6>
+                                <h1 className="card-text">{musicians?.mband}</h1>
+                                <h1 className="card-text">{musicians?.mbio}</h1>
+                            </div>
+                            <div>
+                                {/* <Link to={`/admin/${musicians?.mid}`}>
+                        <button className="btn btn-sm btn-primary">Admin Options</button>
+                    </Link> */}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="d-flex flex-column column h-100">
+                    <div id="card-container">
+                        <div className="card m-3 d-flex justify-content-between align-items-center flex-row yop">
+                            <div className="card-body">
+                                <h6 className="card-subtitle mb-2 text-muted">
+                                    @{musicians?.mname}
+                                </h6>
+                                {musicians?.mdob ? <h1 className="card-text">{moment(musicians?.mdob).format("LL")}</h1> : null}
+                                <h1 className="card-text">{musicians?.mzodiac}</h1>
+                                <h1 className="card-text">{musicians?.mhometown}</h1>
+                                <h1 className="card-text">{musicians?.mage}</h1>
+                            </div>
+                            <div>
+                                {/* <Link to={`/admin/${props.musicians.mid}`}>
+                        <button className="btn btn-sm btn-primary">Admin Options</button>
+                    </Link> */}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 interface IArtistProps {
     musician: {
-        mid: string
-        mname: string,
-        mband: string,
-        mbio: string,
-        mdob: string,
-        mzodiac: string,
-        mspotifyid: string,
-        mhometown: string,
-        mage: string
-    }
+        mid: string;
+        mname: string;
+        mband: string;
+        mbio: string;
+        mdob: string;
+        mzodiac: string;
+        mspotifyid: string;
+        mhometown: string;
+        mage: string;
+    };
 }
 
-export default Artist
+export default Artist;
